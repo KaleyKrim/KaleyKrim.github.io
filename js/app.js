403bdb41f364b9fb1eb28dea557878358108ccc0
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-12-10 18:55:17 -1000

    sticky footer

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@ const contentDiv = document.getElementById('words');
 
 const aboutButton = document.getElementById('about');
 const portfolioButton = document.getElementById('portfolio');
-const resumeButton = document.getElementById('resume');
 const contactButton = document.getElementById('contact');
-const extrasButton = document.getElementById('extras');
 
 aboutButton.addEventListener('click', ()=>{
   contentDiv.innerHTML = snippets.about;
@@ -14,9 +12,9 @@ portfolioButton.addEventListener('click', ()=>{
   contentDiv.innerHTML = snippets.portfolio;
 });
 
-// contactButton.addEventListener('click', ()=>{
-//   contentDiv.innerHTML = snippets.contact;
-// });
+contactButton.addEventListener('click', ()=>{
+  contentDiv.innerHTML = snippets.contact;
+});
 
 window.onscroll = function() {myFunction()};
 
--- js/contentSnippets.js
+++ js/contentSnippets.js
@@ -3,11 +3,9 @@ const snippets = (function(){
 
   const about = `
     <div id = "sub-header">
-      <h1>About Kaley</h1>
+      <h1>About Me</h1>
     </div>
-    <div class="post-it-board">
       <div class = "bio-block">
-        <div class="tape"></div>
         <blockquote>
           I graduated with a BA in East Asian Languages and Literatures from the University of Florida in 2011, and spent the following three years as an assistant language teacher in a snowy town in Northern Japan. After returning to the US, I began my career as a translator, and devoted the next three years to translating Japanese games, visual novels, short stories, and news articles into English.
           </blockquote>
@@ -15,11 +13,6 @@ const snippets = (function(){
             <blockquote>
           In early 2017, I decided to tear myself away from translation, and made the switch to web development.
           </blockquote>
-
-        </div>
-        <div id="ecuador"></div>
-        <div id="volcano"></div>
-        <div id="igloo"></div>
       </div>
     </div>
   `;
@@ -41,8 +34,7 @@ const snippets = (function(){
   <div id = "sub-header">
       <h1>Contact Me</h1>
   </div>
-  <div class = "text-block">
-  <div class="tape"></div>
+  <div class = "bio-block">
   <ul>
     <li>
       LinkedIn: <a href="https://linkedin.com/in/kaleykriminger/">linkedin.com/in/kaleykriminger/</a>
